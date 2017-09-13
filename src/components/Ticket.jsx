cef3aab2c8e985f1c5c2c7e46cd85e50bda6b557
Calla Rudolph <callarudolph@gmail.com>
2017-09-12 20:11:59 -0700

    refactor components and model to remove timeOpened property

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Moment from "moment";
 
 function Ticket(props) {
   return (
       <div>
           <h3>{props.location} - {props.names}</h3>
           <p><em>{props.issue}</em></p>
-          <p>{displayTimeOpen(props.timeOpened)}</p>
           <hr/>
       </div>
   );
-}
-
-function displayTimeOpen(timeOpened) {
-  let now = new Moment(new Date());
-  let then = new Moment(timeOpened);
-  let stringTime = then.from(now, true);
-  return stringTime;
 }
 
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string.isRequired,
-  timeOpened: PropTypes.instanceOf(Date).isRequired
 };
 
 export default Ticket;
--- src/models/ticket.js
+++ src/models/ticket.js
@@ -3,7 +3,6 @@ class Ticket {
     this.names = names;
     this.location = location;
     this.issue = issue;
-    this.timeOpened = new Date();
   }
 }
 
