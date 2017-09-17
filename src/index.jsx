4465d5ef58d18928ad29ca5d95d9d8ce7c846b5f
Calla Rudolph <callarudolph@gmail.com>
2017-09-17 08:57:52 -0700

    create Redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
-
 import App from "./components/App";
+import { createStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+
+const store = createStore(reducer);
 
 const render = (Component) => {
   ReactDOM.render(
