afbb79b9dd9386170cf4bb9f6bbfc9d6bb60d419
Calla Rudolph <callarudolph@gmail.com>
2017-09-17 21:08:39 -0700

    create timeSinceOpened display with Moment

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 import Moment from "moment";
 
 function Ticket(props) {
+  const timeSinceOpened = new Moment().to(props.timeOpened);
+  console.log(timeSinceOpened);
   return (
       <div>
           <h3>{props.location} - {props.names}</h3>
           <p><em>{props.issue}</em></p>
-          <p>{props.timeOpened}</p>
+          <p>{timeSinceOpened}</p>
           <hr/>
       </div>
   );
@@ -17,7 +19,7 @@ Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string.isRequired,
-  timeOpened: PropTypes.string.isRequired
+  timeOpened: PropTypes.number.isRequired
 };
 
 export default Ticket;
