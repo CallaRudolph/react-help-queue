f5160c366d0ef616e86b59a479e3c6847e6e3789
Calla Rudolph <callarudolph@gmail.com>
2017-09-17 21:01:37 -0700

    add wait time in Ticket using Moment and refactor all props to timeOpened, not timeSinceOpened

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Moment from "moment";
 
 function Ticket(props) {
   return (
       <div>
           <h3>{props.location} - {props.names}</h3>
           <p><em>{props.issue}</em></p>
-          <p>{props.timeSinceOpened}</p>
+          <p>{props.timeOpened}</p>
           <hr/>
       </div>
   );
@@ -16,7 +17,7 @@ Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string.isRequired,
-  timeSinceOpened: PropTypes.string.isRequired
+  timeOpened: PropTypes.string.isRequired
 };
 
 export default Ticket;
