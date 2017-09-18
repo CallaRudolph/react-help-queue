35852f85014a653aec0e5f7d606662816ada9bbe
Calla Rudolph <callarudolph@gmail.com>
2017-09-17 20:04:31 -0700

    dispatch action in NewTicketForm to mutate store state

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -3,18 +3,28 @@ import Ticket from "../models/Ticket.js";
 import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
+import c from "./../constants";
 
 class NewTicketForm extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log(props);
     this.handleNewTicketFormSubmission = this.handleNewTicketFormSubmission.bind(this);
   }
 
   handleNewTicketFormSubmission(event) {
     event.preventDefault();
     const { _names, _location, _issue } = this.refs;
+    const { dispatch } = this.props;
+    const action = {
+      type: c.ADD_TICKET,
+      id: null,
+      names: _names.value,
+      location: _location.value,
+      description: _issue.value,
+      timeOpened: new Date().getTime()
+    }
+    dispatch(action);
     var newTicket = new Ticket(_names.value, _location.value, _issue.value);
     this.props.onNewTicketCreation(newTicket);
     this.props.hideFormAfterSubmission();
