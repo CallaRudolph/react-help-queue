636b0179071c6d854f64526a79c99316a2b6bbd0
Calla Rudolph <callarudolph@gmail.com>
2017-09-19 20:52:50 -0700

    connect Admin to store and mapStateToProps to list tickets

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TicketList from "./TicketList";
+import {connect} from "react-redux";
 
 class Admin extends React.Component {
   render() {
@@ -12,4 +13,10 @@ class Admin extends React.Component {
   }
 }
 
-export default Admin;
+const mapStateToProps = state => {
+  return {
+    adminMasterTicketList : state
+  };
+};
+
+export default connect(mapStateToProps)(Admin);
