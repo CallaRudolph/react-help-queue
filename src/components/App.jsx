5f1b36f7fd884af3a1e803bcba891aceae1300d6
Calla Rudolph <callarudolph@gmail.com>
2017-09-19 20:37:00 -0700

    import react-router and two routes in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Queue from "./Queue";
+import { Switch, Route } from "react-router-dom";
 
 function App(){
   return (
       <div className="container">
           <Header />
-          <Queue />
+          <Switch>
+              <Route exact path="/" component={Queue} />
+              <Route path="/admin" component={Admin} />
+          </Switch>
       </div>
   );
 }
