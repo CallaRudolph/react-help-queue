5f885a210629b15f3cffce98548d7a9baedc0e05
Calla Rudolph <callarudolph@gmail.com>
2017-09-24 17:11:22 -0700

    create custom middleware to log action in store

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import { AppContainer } from "react-hot-loader";
 import App from "./components/App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers/ticket-list-reducer";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
+import middlewareLogger from "./middleware/middleware-logger";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(middlewareLogger));
 
 ReactDOM.render(
     <Provider store={store}>
